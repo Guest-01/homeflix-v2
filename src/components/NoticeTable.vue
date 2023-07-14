2a57a87ba71326a3d1fc8b1aae452de38eb44d2d
atHome <cloudonly21@gmail.com>
2023-07-14 14:05:08 +0900

    change notice modal

--- src/components/NoticeTable.vue
+++ src/components/NoticeTable.vue
@@ -5,7 +5,7 @@
         <th class="text-left">
           제목
         </th>
-        <th class="text-left">
+        <th class="text-right">
           날짜
         </th>
       </tr>
@@ -13,15 +13,19 @@
     <tbody>
       <tr v-for="item in items">
         <td class="notice-title" @click="openNoticeDetail(item.id)">{{ item.title }}</td>
-        <td>{{ item.updated }}</td>
+        <td class="text-right">{{ item.updated }}</td>
       </tr>
     </tbody>
   </v-table>
   
-  <v-dialog v-model="isOpen">
+  <v-dialog v-model="isOpen" max-width="600px">
     <v-card :title="noticeStore.currentNotice.title" :subtitle="noticeStore.currentNotice.updated">
       <template #text>
         <div v-html="noticeStore.currentNotice.content"></div>
+      </template>
+      <template #actions>
+        <v-spacer></v-spacer>
+        <v-btn @click="isOpen = false" prepend-icon="mdi-close">닫기</v-btn>
       </template>
     </v-card>
   </v-dialog>
