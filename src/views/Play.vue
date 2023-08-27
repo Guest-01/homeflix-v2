8ca4122253478b9237daa9e260813f241cdf7267
atHome <cloudonly21@gmail.com>
2023-08-27 18:22:54 +0900

    add episode chip

--- src/views/Play.vue
+++ src/views/Play.vue
@@ -27,6 +27,22 @@
         </v-card>
       </v-col>
     </v-row>
+
+    <v-row dense>
+      <v-col cols="12">
+        <v-card>
+          <v-card-subtitle class="mt-4">전체 회차</v-card-subtitle>
+          <v-card-text class="pt-0">
+            <v-chip-group>
+              <v-chip v-for="v in videoStore.videos" :key="v.id"
+                :to="{ name: 'Episode', params: { titleId: currentVid?.expand.title.id, videoId: v.id } }">
+                {{ v.episode }}화 {{ v.on_air }}
+              </v-chip>
+            </v-chip-group>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -49,6 +65,11 @@ export default defineComponent({
   async created() {
     await this.initData();
   },
+  // 다른 회차를 눌렀을 때, 동일한 route에 param만 달라지는거라 새로고침이 일어나지 않음
+  // 따라서 watch에 route의 param이 바뀌면 다시 실행할 함수를 넣어줘야함
+  watch: {
+    $route: "initData"
+  },
   methods: {
     async initData() {
       // 타이틀의 모든 비디오(에피소드)를 가져온다 (최대 200건이 기본값)
@@ -60,10 +81,10 @@ export default defineComponent({
       } else {
         this.currentVid = this.videoStore.videos.filter(el => el.id === this.$route.params.videoId)[0];
       }
-      
+
       // 파일 URL 가져오기
       this.src = await this.videoStore.getVideoSrc(this.currentVid);
-      
+
       // 조회수 1 증가시키기
       await this.videoStore.incrementViewCount(this.currentVid);
     },
